ab10eb6d151310355ccdb9956052b3d8458df603
Fábio Souza <fabio.souza@vaipe.com.br>
2020-07-29 22:09:00 -0300

    Adds register category form

--- src/pages/Register/Category/index.js
+++ src/pages/Register/Category/index.js
@@ -1,25 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Template from '../../../components/Template';
+import FormField from '../../../components/FormField';
 
 function RegisterCategory() {
+  const initCategory = {
+    name: '',
+    description: '',
+    color: '',
+  }
+  const [categories, setCategories] = useState([])
+  const [category, setCategory] = useState(initCategory)
+
+  function addCategory(input, value) {
+    setCategory({
+      ...category,
+      [input]: value,
+    })
+  }
+
+  function handleCategory(event) {
+    addCategory(
+      event.target.getAttribute('name'),
+      event.target.value
+    )
+  }
+
   return (
     <Template>
-      <h1>Cadastro de categorias</h1>
+      <h1>Cadastro de categorias | {category.name}</h1>
 
-      <form>
+      <form onSubmit={(event) => {
+        event.preventDefault()
+        setCategories([
+          ...categories,
+          category
+        ])
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-          />
-        </label>
+        setCategory(initCategory)
+      }}>
+
+        <FormField
+          label="Nome da categoria"
+          type="text"
+          name="name"
+          value={category.name}
+          onChange={handleCategory}
+        />
+
+        <div>
+          <label>
+            Descrição:
+            <textarea
+              name="description"
+              value={category.description}
+              onChange={handleCategory}
+            />
+          </label>
+        </div>
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="color"
+          value={category.color}
+          onChange={handleCategory}
+        />
 
         <button>
           Cadastrar
         </button>
       </form>
+
+      <ul>
+        {categories.map((category, i) => {
+          return (
+            <li key={i}>
+              {category.name}
+            </li>
+          )
+        })}
+      </ul>
 
       <Link to="/">
         Ir para home
